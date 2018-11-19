ab559c562cebb2c321c4fbdadbb535f7b9bbd106
MELVERTON\Mel <huntermelverton@gmail.com>
2018-11-19 16:57:29 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import moment from 'moment';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      data: {},
+      inputValue: '',
+      isLoading: true
+    }
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.inputValue}&appid=b3456f9acbfa64fc4495e6696ecdc9a5`)
+      .then((response) => {
+        console.log(response)
+        this.setState({
+          data: response.data,
+          isLoading: false
+        })
+      })
+      .catch((error) => {
+        // Error
+        console.log(error)
+       });
+  } 
+  renderWeather() {
+    let { wind, main, weather, name  } = this.state.data;
+    let fahrenheit = Math.round(((main.temp - 273.15)*1.8)+32); //convert kelvin into fahrenheit then round it to a nice whole number
+    let description = weather[0].description;
+    let icon = weather[0].icon;
+    icon = `http://openweathermap.org/img/w/${icon}.png`;
+    return (
+      <div className="wrapper">
+        <h1 className="location">{name}</h1>
+        <h2 className="date">{moment().format('MMMM, Do YYYY')}</h2>
+        <div className="container">
+          <img src={icon} alt="open weather icon" />
+          <div className="conditions">{description}</div>
+        </div>
+        <p className="temp">{fahrenheit}&#176;F</p>
+        <div>wind speed of {wind.speed} m/s</div>
+        <div>humidity of {main.humidity}%</div>
+      </div>
+    )
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <Input
+            placeholder="Brooklyn"
+            value={this.state.inputValue}
+            onChange={e => this.setState({ inputValue: e.target.value})}
+            inputProps={{
+              'aria-label': 'Description',
+            }}
+          />
+        </form>
+        {this.state.isLoading ? <div></div> : this.renderWeather()}
       </div>
     );
   }
 }
 
 export default App;
+
+/*todo 
+You should render the weather icon that comes back with
+the response, the description of the weather, the temperature in Fahrenheit, wind speed and humidity.
+*/
+
---
+++ src/config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  weatherAPI:'http://api.openweathermap.org/data/2.5/weather?zip=11234,us&appid=b3456f9acbfa64fc4495e6696ecdc9a5',
+  weatherAPI:'http://api.openweathermap.org/data/2.5/weather?q=brooklyn,us&appid=b3456f9acbfa64fc4495e6696ecdc9a5'
+}
